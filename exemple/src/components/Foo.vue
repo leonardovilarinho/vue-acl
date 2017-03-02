a00a730552581d561e74112ee93eaa667c0cf1c1
Samuel Desconsi <Samuel Desconsi>
2017-03-02 14:45:56 -0300

    Add exemple code

---
+++ exemple/src/components/Foo.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="">
+    <h1>Foo</h1>
+    <button type="button" name="foo" @click="goIndex()">Go Index</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'foo-component',
+  methods: {
+    goIndex () {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
